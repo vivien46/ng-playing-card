8d43866e2f5937e7b810d57e4375c03be36407d5
vivien <vlad-vivien@orange.fr>
2024-04-17 15:07:15 +0200

    commit with first card components settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardsComponent } from './components/playing-cards/playing-cards.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PlayingCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'playing-card';
+  
 }
---
+++ src/app/components/playing-cards/playing-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './playing-cards.component.html',
+  styleUrl: './playing-cards.component.css'
+})
+export class PlayingCardsComponent {
+
+}
